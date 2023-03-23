8d76b1f6bffbb39b110711eb3cc4145fecd7f8b4
Yusuf-R <y.abdulwasiu@gmail.com>
2023-03-23 01:56:28 +0100

    feat: :sparkles: enrich readability

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,9 @@
 from models.engine.file_storage import FileStorage
 from os import getenv
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
+db_storage_type = getenv("HBNB_TYPE_STORAGE")
 
-if storage_type == 'db':
+if db_storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
